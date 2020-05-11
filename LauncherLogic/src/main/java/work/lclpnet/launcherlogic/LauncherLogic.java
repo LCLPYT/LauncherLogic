c90f1c1adbcfa5555511a5c6ea76fdf0c04befa3
LCLP <lukas@lclpnet.work>
2020-05-11 17:10:31 +0200

    setup #1
    (now ignoring gradle.properties, since it contains a machine specific
    path to jdk 13)

--- LauncherLogic/src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
+++ LauncherLogic/src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
@@ -4,6 +4,14 @@ public class LauncherLogic {
 
 	public static void main(String[] args) {
 		System.out.println("Using Java version " + System.getProperty("java.version") + "...");
+		
+		if(args.length > 0) {
+			System.err.println("welp. an argument was passed...");
+			System.exit(1);
+			return;
+		}
+		
+		System.out.println("Hello from java!");
 	}
 	
 	public static boolean test() {
