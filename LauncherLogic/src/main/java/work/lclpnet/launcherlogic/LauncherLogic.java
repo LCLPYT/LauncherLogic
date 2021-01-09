a87b9f0ec5d380b676a2c44b2e5c7804dc3b6d3d
LCLP <lukas@lclpnet.work>
2021-01-09 17:18:08 +0100

    logging

--- LauncherLogic/src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
+++ LauncherLogic/src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
@@ -1,5 +1,10 @@
 package work.lclpnet.launcherlogic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
@@ -8,6 +13,7 @@ import work.lclpnet.launcherlogic.cmd.CommandCheckUpdate;
 import work.lclpnet.launcherlogic.cmd.CommandEcho;
 import work.lclpnet.launcherlogic.cmd.CommandInstall;
 import work.lclpnet.launcherlogic.cmd.CommandPreparePlay;
+import work.lclpnet.launcherlogic.util.LoggingPrintStream;
 import work.lclpnet.launcherlogic.util.ProgressCallbackClient;
 
 @Command(
@@ -27,11 +33,34 @@ public class LauncherLogic implements Callable<Integer>{
 	public static final String VERSION = "1.0";
 	public static final boolean DEBUG = true;
 	private static LauncherLogic instance = null;
-
+	
 	public static void main(String[] args) {
 		//args = new String[] {"install", "ls5", "/home/lukas/lclpserver", "--debug", "--java-exe", "/home/lukas/Documents/projects/lclpserver5/LCLPLauncher/bin/launcherlogic/runtime/bin/java", "--launcher-forge-installer-jar", "/home/lukas/Documents/projects/lclpserver5/LCLPLauncher/bin/launcherlogic/launcherlogic-forge_installer.jar", "--profile-java", "/usr/lib/jvm/java-8-openjdk-amd64/bin/java"};
 		//args = new String[] {"install", "ls5", "C:\\Users\\Lukas\\lclpserver5", "--debug", "--java-exe", "C:\\Users\\Lukas\\Documents\\Electron\\lclplauncher\\bin\\launcherlogic\\runtime\\bin\\java.exe", "--launcher-forge-installer-jar", "C:\\Users\\Lukas\\Documents\\Electron\\lclplauncher\\bin\\launcherlogic\\launcherlogic-forge_installer.jar"};
-		System.out.println("Running LauncherLogic version " + VERSION + " using java " + System.getProperty("java.version"));
+		
+		File tempDir = new File(System.getProperty("java.io.tmpdir"), "launcherlogic");
+		tempDir.mkdirs();
+		File logFile = new File(tempDir, String.format("launcherlogic_%s_stdout.txt", System.currentTimeMillis() / 1000L));
+		File errFile = new File(tempDir, String.format("launcherlogic_%s_stderr.txt", System.currentTimeMillis() / 1000L));
+		
+		System.out.println("Writing logs to: " + tempDir.getAbsolutePath());
+		
+		FileOutputStream outOut, outErr;
+		try {
+			outOut = new FileOutputStream(logFile);
+			outErr = new FileOutputStream(errFile);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			System.exit(1);
+			return;
+		}
+		
+		PrintStream origOut = System.out, origErr = System.err;
+		
+		System.setOut(new LoggingPrintStream(origOut, outOut));
+		System.setErr(new LoggingPrintStream(origErr, outErr));
+
+		System.out.printf("Running LauncherLogic version %s using java %s\n", VERSION, System.getProperty("java.version"));
 
 		if(args.length <= 0) System.out.println("Supply '--help' as argument to get help.");
 
@@ -45,6 +74,16 @@ public class LauncherLogic implements Callable<Integer>{
 			}
 			ProgressCallbackClient.closeAllSockets();
 		}
+		
+		try {
+			outOut.close();
+			outErr.close();
+			System.setOut(origOut);
+			System.setErr(origErr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		System.exit(exitCode);
 	}
 
---
+++ LauncherLogic/src/main/java/work/lclpnet/launcherlogic/util/LoggingPrintStream.java
@@ -0,0 +1,195 @@
+package work.lclpnet.launcherlogic.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+public class LoggingPrintStream extends PrintStream {
+
+	protected OutputStream loggingStream;
+
+	public LoggingPrintStream(OutputStream out, OutputStream loggingStream) {
+		super(out, true);
+		this.loggingStream = loggingStream;
+	}
+
+	protected void newLine() {
+		try {
+			loggingStream.write(new byte[] {10});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeString(String x) {
+		try {
+			loggingStream.write(x.getBytes(StandardCharsets.UTF_8));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeBoolean(boolean b) {
+		writeString(String.valueOf(b));
+	}
+
+	private void writeChar(char c) {
+		writeString(String.valueOf(c));
+	}
+
+	private void writeCharArray(char[] c) {
+		writeString(String.valueOf(c));
+	}
+
+	private void writeDouble(double d) {
+		writeString(String.valueOf(d));
+	}
+	
+	private void writeFloat(float f) {
+		writeString(String.valueOf(f));
+	}
+	
+	private void writeInt(int i) {
+		writeString(String.valueOf(i));
+	}
+	
+	private void writeLong(long l) {
+		writeString(String.valueOf(l));
+	}
+	
+	private void writeObj(Object o) {
+		writeString(o.toString());
+	}
+
+	@Override
+	public void println() {
+		super.println();
+		newLine();
+	}
+
+	@Override
+	public void print(String s) {
+		super.print(s);
+		writeString(s);
+	}
+
+	@Override
+	public void println(String x) {
+		super.println(x);
+		writeString(x);
+		newLine();
+	}
+
+	@Override
+	public void print(boolean b) {
+		super.print(b);
+		writeBoolean(b);
+	}
+
+	@Override
+	public void println(boolean x) {
+		super.println(x);
+		writeBoolean(x);
+		newLine();
+	}
+
+	@Override
+	public void print(char c) {
+		super.print(c);
+		writeChar(c);
+	}
+
+	@Override
+	public void println(char x) {
+		super.println(x);
+		writeChar(x);
+		newLine();
+	}
+
+	@Override
+	public void print(char[] s) {
+		super.print(s);
+		writeCharArray(s);
+	}
+
+	@Override
+	public void println(char[] x) {
+		super.println(x);
+		writeCharArray(x);
+		newLine();
+	}
+
+	@Override
+	public void print(double d) {
+		super.print(d);
+		writeDouble(d);
+	}
+	
+	@Override
+	public void println(double x) {
+		super.println(x);
+		writeDouble(x);
+		newLine();
+	}
+	
+	@Override
+	public void print(float f) {
+		super.print(f);
+		writeFloat(f);
+	}
+	
+	@Override
+	public void println(float x) {
+		super.println(x);
+		writeFloat(x);
+		newLine();
+	}
+	
+	@Override
+	public void print(int i) {
+		super.print(i);
+		writeInt(i);
+	}
+	
+	@Override
+	public void println(int x) {
+		super.println(x);
+		writeInt(x);
+		newLine();
+	}
+	
+	@Override
+	public void print(long l) {
+		super.print(l);
+		writeLong(l);
+	}
+	
+	@Override
+	public void println(long x) {
+		super.println(x);
+		writeLong(x);
+		newLine();
+	}
+	
+	@Override
+	public void print(Object obj) {
+		super.print(obj);
+		writeObj(obj);
+	}
+	
+	@Override
+	public void println(Object x) {
+		super.println(x);
+		writeObj(x);
+		newLine();
+	}
+	
+	@Override
+	public PrintStream printf(Locale l, String format, Object... args) {
+		writeString(String.format(l, format, args));
+		return super.printf(l, format, args);
+	}
+
+}
