8107e5a8bee814c9ae6b2421b1844875e7b2b356
LCLP <lukas@lclpnet.work>
2021-06-06 01:18:39 +0200

    fixed dumb error where arguments are overridden

--- src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
+++ src/main/java/work/lclpnet/launcherlogic/LauncherLogic.java
@@ -14,7 +14,6 @@ import java.util.concurrent.Callable;
 @Command(
         name = "launcher_logic",
         mixinStandardHelpOptions = true,
-        version = LauncherLogic.VERSION,
         description = "Main command for launcher logic.",
         subcommands = {
                 CommandEcho.class,
@@ -25,15 +24,13 @@ import java.util.concurrent.Callable;
 )
 public class LauncherLogic implements Callable<Integer> {
 
-    public static final String VERSION = "1.0";
-
     public static void main(String[] args) {
         // args = new String[] {"install", "ls5", "/home/lukas/lclpserver", "--debug", "--java-exe", "/home/lukas/Documents/projects/lclpserver5/LCLPLauncher/bin/launcherlogic/runtime/bin/java", "--launcher-forge-installer-jar", "/home/lukas/Documents/projects/lclpserver5/LCLPLauncher/bin/launcherlogic/launcherlogic-forge_installer.jar", "--profile-java", "/usr/lib/jvm/java-8-openjdk-amd64/bin/java"};
-        args = new String[]{"install", "ls5", "C:\\Users\\Lukas\\lclpserver5", "--debug", "--java-exe", "C:\\Users\\lukas\\Documents\\projects\\misc\\LCLPLauncher\\bin\\launcherlogic\\runtime\\bin\\java.exe", "--launcher-forge-installer-jar", "C:\\Users\\lukas\\Documents\\projects\\misc\\LCLPLauncher\\bin\\launcherlogic\\launcherlogic-forge_installer.jar"};
+        // args = new String[]{"install", "ls5", "C:\\Users\\Lukas\\lclpserver5", "--debug", "--java-exe", "C:\\Users\\lukas\\Documents\\projects\\misc\\LCLPLauncher\\bin\\launcherlogic\\runtime\\bin\\java.exe", "--launcher-forge-installer-jar", "C:\\Users\\lukas\\Documents\\projects\\misc\\LCLPLauncher\\bin\\launcherlogic\\launcherlogic-forge_installer.jar"};
 
         Logging.setupLogging();
 
-        System.out.printf("Running LauncherLogic version %s using java %s\n", VERSION, System.getProperty("java.version"));
+        System.out.printf("Running LauncherLogic version %s using java %s\n", "1.1.1", System.getProperty("java.version"));
 
         if (args.length <= 0) System.out.println("Supply '--help' as argument to get help.");
 
